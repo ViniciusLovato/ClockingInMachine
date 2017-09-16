6f1bbfb69e74c661b6379f4a2d98dc68f58a774c
Vinicius Lovato <lovato.vinicius@gmail.com>
2017-09-16 01:26:57 -0300

    Added LCD and RFID

---
+++ bater.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+sleep 3
+echo 1
+
+
--- clockIn.ino
+++ clockIn.ino
@@ -1,87 +1,97 @@
 
-// constants won't change. They're used here to set pin numbers:
-const int buttonPin2 = 2;    // lovato
-const int buttonPin3 = 3;    // compri
-const int buttonPin4 = 4;    // rebecca
-const int buttonPin5 = 5;    // omoto
+#include <SPI.h>
+#include <MFRC522.h>
+#include <LiquidCrystal.h>
 
-const int successLed = 13;      
-const int loadingLed = 12;
-const int failLed = 7;
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 mfrc522(SS_PIN, RST_PIN);  // Create MFRC522 instance.
+
+LiquidCrystal lcd(6, 7, 5, 4, 3, 2); 
+
+char st[20];
 
 int incomingByte = 0;
 
+
 void setup() 
 {
-  pinMode(buttonPin2, INPUT);
-  pinMode(buttonPin3, INPUT);
-  pinMode(buttonPin4, INPUT);
-  pinMode(buttonPin5, INPUT);
+  pinMode(8,OUTPUT); // buzzer pin
+  Serial.begin(9600); // Initialize Serial port comm
+  SPI.begin();    // Initialize SPI bus
+  mfrc522.PCD_Init(); //  MFRC522 (RFID reader)
 
- 
-  pinMode(successLed, OUTPUT);
-  pinMode(loadingLed, OUTPUT);
-  pinMode(failLed, OUTPUT);
-
-  Serial.begin(9600);
-  // set initial LED state
-  digitalWrite(successLed, LOW);
+  // Define number of rows and columns
+  lcd.begin(16, 2);  
+  lcd.print("Aproxime seu");
+  lcd.setCursor(0,1);
+  lcd.print("cartao");
 }
 
-void loop()
+void loop() 
 {
-  // read the state of the switch into a local variable:
-  int reading2 = digitalRead(buttonPin2);
-  int reading3 = digitalRead(buttonPin3);
-  int reading4 = digitalRead(buttonPin4);
-  int reading5 = digitalRead(buttonPin5);
-
   
-  // We can create a function here to handle all the possible buttons (aka users)
-  // we can also ser some constants for each person
-  // send a signal to clock in
-  if (reading2 == HIGH) 
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
   {
-    digitalWrite(loadingLed, HIGH);
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  String content = "";
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  content.toUpperCase();
+  if (content.substring(1) == "6A 31 0F BB") // lovato card
+  {
+    tone(8,300,300); //MI
+    delay(500);
+    tone(8,300,300); //MI
     Serial.println("lovato");
-  }
-  else if(reading3 == HIGH)
-  {
-    digitalWrite(loadingLed, HIGH);
-    Serial.println("compri");
-  }
-  else if(reading4 == HIGH)
-  {
-    digitalWrite(loadingLed, HIGH);
-    Serial.println("rebecca");
-  }
-  else if(reading5 == HIGH)
-  {
-    digitalWrite(loadingLed, HIGH);
-    Serial.println("omoto");
-  }
-
- 
-  // get the result from the last request
-  // If the clocking in was performed correctly we light up the green led
+    lcd.clear();
+    lcd.setCursor(0,0);
+    lcd.print("Tentativa para");
+    lcd.setCursor(0,1);
+    lcd.print("Lovato");
+    delay(3000);
+    lcd.clear();
   
-    // Avoid too much serial reading, arduino is realy slow
+    incomingByte = Serial.parseInt();
+    if(incomingByte == 1)
+    {
+     
+      tone(8,300,300); 
+      lcd.print("Ponto registrado");
+      
+    }
+    else if(incomingByte == 2)
+    {
+      lcd.print("Erro no registro");
+    }
+    delay(3000);
+  }
+  else 
+  {
+    tone(8,262,200); 
+    lcd.clear();
+    lcd.print("Cartao");
+    lcd.setCursor(0,1);
+    lcd.print("invalido");
+    delay(3000);
+    
+  }
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print("Aproxime seu");
+  lcd.setCursor(0,1);
+  lcd.print("cartao");
   delay(500);
-  incomingByte = Serial.parseInt();
-  if(incomingByte == 1)
-  {
-    digitalWrite(loadingLed, LOW); 
-    digitalWrite(successLed, HIGH);
-    delay(2000);
-    digitalWrite(successLed, LOW);
-  }
-  else if(incomingByte == 2)
-  {
-    digitalWrite(loadingLed, LOW);
-    digitalWrite(failLed, HIGH);
-    delay(2000);
-    digitalWrite(failLed, LOW);  
-  }
-}
+} 
 
 
